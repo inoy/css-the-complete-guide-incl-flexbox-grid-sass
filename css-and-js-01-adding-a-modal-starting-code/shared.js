1412376c6c14327b7b5ed01a557905cc191c487e
inoy <7m7110123@gmail.com>
2022-03-02 17:28:17 +0900

    Adding an Event Listener

--- css-and-js-01-adding-a-modal-starting-code/shared.js
+++ css-and-js-01-adding-a-modal-starting-code/shared.js
@@ -1,4 +1,11 @@
 const backdrop = document.getElementsByClassName('backdrop')[0];
+const modal = document.getElementsByClassName('modal')[0];
+const selectPlanButtons = [...document.getElementsByClassName('plan')]
+  .flatMap(plan => [...plan.getElementsByClassName('button')]);
 
-// console.log(backdrop);
-backdrop.style.display = 'block';
+selectPlanButtons.forEach(selectPlanButton =>
+  selectPlanButton.addEventListener('click', () => {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+  })
+);
