0df9838ab7f7308f6a2f35ffb7d2dd9a27f0d554
inoy <7m7110123@gmail.com>
2022-03-02 17:35:45 +0900

    Time to Practice - Adding Styles with JavaScript

--- css-and-js-01-adding-a-modal-starting-code/shared.js
+++ css-and-js-01-adding-a-modal-starting-code/shared.js
@@ -1,11 +1,21 @@
 const backdrop = document.getElementsByClassName('backdrop')[0];
 const modal = document.getElementsByClassName('modal')[0];
+
+const openModal = () => {
+  backdrop.style.display = 'block';
+  modal.style.display = 'block';
+};
+
+const closeModal = () => {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+};
+
 const selectPlanButtons = [...document.getElementsByClassName('plan')]
   .flatMap(plan => [...plan.getElementsByClassName('button')]);
-
 selectPlanButtons.forEach(selectPlanButton =>
-  selectPlanButton.addEventListener('click', () => {
-    backdrop.style.display = 'block';
-    modal.style.display = 'block';
-  })
+  selectPlanButton.addEventListener('click', openModal)
 );
+
+const modalActionNegativeButton = document.getElementsByClassName('modal__action--negative')[0];
+modalActionNegativeButton.addEventListener('click', closeModal);
