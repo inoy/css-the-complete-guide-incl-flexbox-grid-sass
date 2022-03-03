48a684c99bc9172ad2b7c8761308e09c08f25532
inoy <7m7110123@gmail.com>
2022-03-03 10:31:14 +0900

    Manipulating Element Classes

--- css-and-js-01-adding-a-modal-starting-code/shared.js
+++ css-and-js-01-adding-a-modal-starting-code/shared.js
@@ -3,31 +3,31 @@ const modal = document.getElementsByClassName('modal')[0];
 const mobileNav = document.getElementsByClassName('mobile-nav')[0];
 
 const openModal = () => {
-  backdrop.style.display = 'block';
-  modal.style.display = 'block';
+  backdrop.classList.add('open');
+  modal.classList.add('open');
 };
 
 const closeModal = () => {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  backdrop.classList.remove('open');
+  modal.classList.remove('open');
 };
 
 backdrop.addEventListener('click', () => {
-  mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
   closeModal();
 });
 
 const selectPlanButtons = [...document.getElementsByClassName('plan')]
   .flatMap(plan => [...plan.getElementsByClassName('button')]);
-selectPlanButtons.forEach(selectPlanButton =>
-  selectPlanButton.addEventListener('click', openModal)
-);
+selectPlanButtons.forEach(selectPlanButton => {
+  selectPlanButton.addEventListener('click', openModal);
+});
 
 const modalActionNegativeButton = document.getElementsByClassName('modal__action--negative')[0];
 modalActionNegativeButton.addEventListener('click', closeModal);
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 toggleButton.addEventListener('click', () => {
-  mobileNav.style.display = 'block';
-  backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 });
